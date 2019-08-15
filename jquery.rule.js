6b836a024bdaa2c2c04d0a97f4f9673db6c8c985
Mark Brewsala <mbrewsala@panopto.com>
2019-08-14 19:04:57 -0700

    correct logical departure of hasOwnProperty
    We observed behavior differences from the earlier property existence check versus `hasOwnProperty`, which is sensitive to inherited properties. This change restores the earlier functional test without fully dereferencing the property.

--- jquery.rule.js
+++ jquery.rule.js
@@ -84,7 +84,7 @@
 				case 'object':
 					if( ss[0] ) ss = ss[0];
 					if( ss[sheet] ) ss = ss[sheet];
-					if( ss.hasOwnProperty(rules) ) break;//only if the stylesheet is valid
+					if( rules in ss ) break;//only if the stylesheet is valid
 				default:
 					if( typeof r == 'object' ) return r;//let's not waist time, it is parsed
 					ss = storage;
